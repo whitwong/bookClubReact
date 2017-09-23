a7c335917f1dd8d59c8a4f43d1635a1e22195797
Whitney Wong <whitney.wong@utexas.edu>
2017-09-23 17:47:06 -0500

    package.json - Added axios and firebase dependencies. app.js - Added firebase reference Groups.js - Setup layout for Discussion component routes.js - Added route for Discussion Added helper and component files for Discussions.

--- my-app/app/app.js
+++ my-app/app/app.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Routes from '../config/routes.js'
+import Routes from '../config/routes.js';
+import * as firebase from 'firebase';
+
+// const config = {
+//   apiKey: "AIzaSyBUVyIW2d33WHzArLsdPx3X-X39qV-SZLY",
+//   authDomain: "bookclub-ed08b.firebaseapp.com",
+//   databaseURL: "https://bookclub-ed08b.firebaseio.com",
+//   projectId: "bookclub-ed08b",
+//   storageBucket: "bookclub-ed08b.appspot.com",
+//   messagingSenderId: "874403788158"
+// };
+  const config = {
+    apiKey: "AIzaSyBTEYvtoqeo5iHwWDim_rif0GnSvep2mXY",
+    authDomain: "trainhw-7c836.firebaseapp.com",
+    databaseURL: "https://trainhw-7c836.firebaseio.com",
+    projectId: "trainhw-7c836",
+    storageBucket: "trainhw-7c836.appspot.com",
+    messagingSenderId: "339587231309"
+  };
+firebase.initializeApp(config);
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
